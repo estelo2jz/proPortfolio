ca80fbb45f3f3124be289a19b54c0805daf3ef19
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-11-01 14:32:28 -0600

    added skills logo

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import Picme from "../logo/pic.jpg";
 
+import HTML from '../logo/skills/html-5.svg'
+import CSS from '../logo/skills/css.svg';
+import SCSS from '../logo/skills/sass.svg';
+import JavaScript from '../logo/skills/javascript.svg';
+import Python from '../logo/skills/python.svg';
+import ReactSkill from '../logo/skills/react.svg';
+import NodeJS from '../logo/skills/nodejs.svg';
+import MySQL from '../logo/skills/mysql.svg';
+
 export default class About extends Component {
   render() {
     return (
@@ -14,6 +23,18 @@ export default class About extends Component {
                 I like to code starting from scratch, and see where it leads me. 
                 I like to animate things and bringing ideas to life in the browser. I value simple content structure, clean design patterns, and thoughtful user experience.
               </p>
+              <div className="main__skills-container">
+                <ul>
+                  <li><img src={HTML} /><p>HTML</p></li>
+                  <li><img src={CSS} /><p>CSS</p></li>
+                  <li><img src={SCSS} /><p>SCSS</p></li>
+                  <li><img src={JavaScript} /><p>JavaScript</p></li>
+                  <li><img src={ReactSkill} /><p>ReactJS</p></li>
+                  <li><img src={Python} /><p>Python</p></li>
+                  <li><img src={NodeJS} /><p>NodeJS</p></li>
+                  <li><img src={MySQL} /><p>MySQL</p></li>
+                </ul>
+              </div>
             </div>
             <div className="main__image">
               <img src={Picme} alt="logo" className="main__img" />
