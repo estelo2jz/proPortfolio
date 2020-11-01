faadf2fd425de48fc6ab2618a43f4e214b1099aa
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-11-01 12:57:51 -0600

    initial commit, had to copy everything from other folder of portfolioV4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import React from 'react';
+import Nav from './components/Nav';
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
-import './App.css';
+import '../src/styles/main.scss';
 
 function App() {
   return (
     <div className="App">
-     hhelo
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header> */}
+      <Nav />
+      <Home />
+      <About />
+      {/* <Services /> */}
+      <Portfolio />
+      <Contact />
+      <Footer />
     </div>
   );
 }
