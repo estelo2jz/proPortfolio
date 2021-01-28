4c7d24d13d60c0253fedec8c8a3fdae493b2df89
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2021-01-28 07:54:42 -0600

    styles

---
+++ src/components/About (manitobaaa's conflicted copy 2021-01-04).js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import Picme from "../images/pic.jpg";
+
+import HTML from '../logo/skills/html-5.svg'
+import CSS from '../logo/skills/css.svg';
+import SCSS from '../logo/skills/sass.svg';
+import JavaScript from '../logo/skills/javascript.svg';
+import Python from '../logo/skills/python.svg';
+import ReactSkill from '../logo/skills/react.svg';
+import NodeJS from '../logo/skills/nodejs.svg';
+import MySQL from '../logo/skills/mysql.svg';
+import MongoDB from '../logo/skills/mongo.png';
+import FirebaseLogo from '../logo/skills/firebase.png';
+import VueLogo from '../logo/skills/vue.png';
+
+export default class About extends Component {
+  render() {
+    return (
+      <div className="main__section" id="about">
+        <div className="main__container">
+          <h1>Hello, I'm Estelo!</h1>
+          <div className="main__content">
+            <div className="main__text">
+              <h2>I'm a Web Developer currently living in Beach Park, Illinois.</h2>
+              <p>
+                Proficient in React and Firebase.
+              </p>
+              {/* <p>
+                I like to code starting from scratch, and see where it leads me. 
+                I like to animate things and bringing ideas to life in the browser. I value simple content structure, clean design patterns, and thoughtful user experience.
+              </p> */}
+              <div className="main__skills-container">
+                <ul>
+                  <li><img src={MongoDB} /><p>MongoDB</p></li>
+                  <li><img src={FirebaseLogo} /><p>Firebase</p></li>
+                  <li><img src={HTML} /><p>HTML</p></li>
+                  <li><img src={CSS} /><p>CSS</p></li>
+                  <li><img src={SCSS} /><p>SCSS</p></li>
+                  <li><img src={JavaScript} /><p>JS</p></li>
+                  <li><img src={VueLogo} /><p>VueJS</p></li>
+                  <li><img src={ReactSkill} /><p>ReactJS</p></li>
+                  <li><img src={Python} /><p>Python</p></li>
+                  <li><img src={NodeJS} /><p>NodeJS</p></li>
+                  <li><img src={MySQL} /><p>MySQL</p></li>
+                </ul>
+              </div>
+            </div>
+            <div className="main__image">
+              <div className="main__img">
+                {/* <img src={Picme} /> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/components/Home (manitobaaa's conflicted copy 2021-01-04).js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Logo1 from '../logo/logo3.png';
+import Picme2 from "../images/me.jpg";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="hero__section" id="home">
+        <div className="hero__wrapper">
+          <div className="column__left">
+            <span>
+              v4 - Estelo Abellanosa
+              {/* <img src={Picme2} alt="logo" className="main__img" /> */}
+            </span>
+            <h1>Front-End Web Developer</h1>
+            <p>
+              I am an experience web developer. I'm a tech enthusiast passionate in codes. 
+              I design, build web applications, and I'm currently looking for new opportunities.</p>
+          </div>
+          <div className="column__right">
+            <img src={Picme2} alt="brain" className="hero__img"/>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
